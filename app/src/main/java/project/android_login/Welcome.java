95119ffb653e1bdf7e16a19c01a87a5f210e5a29
GARUDA <lanka.naveen.emn@gmail.com>
2017-04-30 22:16:22 -0700

    Initial commit

---
+++ app/src/main/java/project/android_login/Welcome.java
@@ -0,0 +1,13 @@
+package project.android_login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Welcome extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome);
+	}
+}
